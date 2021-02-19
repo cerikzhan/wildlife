21460e9868094e2916832b631c01d6b7477ee5ac
Serik <serj_navii@mail.ru>
2021-02-19 06:54:00 +0600

    feat: add working slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Burger menu
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -21,3 +22,34 @@ const navSlide = () => {
 }
 
 navSlide();
+
+// Slider
+const leftBtn = document.querySelector('.left-arrow');
+const rightBtn = document.querySelector('.right-arrow');
+let postsWrapper = document.querySelector('.posts');
+
+let posts = [...document.querySelectorAll('.post')];
+
+let currentPosition = 0;
+
+function setPosition(position){
+    let index = 1;
+    if(1024 >= window.innerWidth && window.innerWidth > 768) index = 2;
+    if(window.innerWidth > 1024) index = 3;
+
+
+    if(position > 0) currentPosition = -(posts.length - index) *100;
+    else if(position < -(posts.length - index) * 100) currentPosition = 0;
+    else currentPosition = position;
+    posts.forEach(slide => slide.style.transform = `translateX(${currentPosition}%)`);
+}
+
+rightBtn.onclick = (e) => {
+    e.preventDefault();
+    setPosition(currentPosition -= 100);
+}
+
+leftBtn.onclick = (e) => {
+    e.preventDefault();
+    setPosition(currentPosition += 100);
+}
